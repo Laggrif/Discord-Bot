9d3919845c9fb6e2781aa4a5553f4e311269e9b8
Laggrif <ugo.novello@epfl.ch>
2024-02-10 19:09:42 +0100

    slightly improved and completed champ infos

--- src/LoL/ChampInfos.py
+++ src/LoL/ChampInfos.py
@@ -16,6 +16,7 @@ FONT100 = FONT60.font_variant(size=100)
 WIDTH = 2120
 HEIGHT = 3769
 
+SPACING = 15
 XS_MARGIN = 10
 S_MARGIN = 20
 M_MARGIN = 40
@@ -37,15 +38,24 @@ def break_text(text: str, font, draw: ImageDraw.ImageDraw, max_width):
     new_text = ''
     tmp = ''
     for word in words:
-        word = ' ' + word
-        tmp += word
-        if draw.textlength(tmp, font) > max_width:
-            tmp = tmp.removesuffix(word)
+        if word == '\n':
+            tmp += word
             new_text += tmp
-            new_text += '\n'
-            tmp = word.removeprefix(' ')
+            tmp = ''
+        else:
+            word = word + ' '
+            tmp += word
+            if draw.textlength(tmp, font) > max_width:
+                tmp = tmp.removesuffix(word)
+                new_text += tmp
+                new_text += '\n'
+                tmp = word.removeprefix(' ')
     new_text += tmp
     return new_text
+
+
+def f_txt(text: str):
+    return text.replace('<br>', ' \n ')
 
 
 def ChI_image(lolData: LoLData, champ: Champ):
@@ -74,30 +84,28 @@ def ChI_image(lolData: LoLData, champ: Champ):
         width = 300
         height = 300
         paste_image(champ_icon_path,
-                    (WIDTH - XS_MARGIN - width, XS_MARGIN),
+                    (WIDTH - M_MARGIN - width, M_MARGIN),
                     (width, height),
                     img,
                     mask=False)
-        champ_icon_box = [WIDTH - XS_MARGIN - width, XS_MARGIN, WIDTH - XS_MARGIN, XS_MARGIN + height]
+        champ_icon_box = [WIDTH - M_MARGIN - width, M_MARGIN, WIDTH - M_MARGIN, M_MARGIN + height]
 
         # -------------------- champ name and title ---------------------
         txt = champ.champ_name() + ' ' + champ.champ_title()
         y = (champ_icon_box[3] - champ_icon_box[1]) / 2 + champ_icon_box[1]
-        draw.text((S_MARGIN, int(y)), txt, font=FONT60)
-        champ_name_box = draw.textbbox((S_MARGIN, S_MARGIN), txt, font=FONT60)
+        draw.text((S_MARGIN, int(y)), txt, font=FONT60.font_variant(size=80))
+        champ_name_box = draw.textbbox((S_MARGIN, S_MARGIN), txt, font=FONT60.font_variant(size=80))
 
         # -------------------- lore ---------------------
-        txt = break_text(champ.champ_lore(), FONT40, draw, WIDTH - 2 * L_MARGIN)
-        coords = (L_MARGIN, champ_icon_box[3] + XL_MARGIN)
-        lore_text_box = draw.multiline_textbbox(coords, txt, font=FONT40)
-        draw.rectangle((lore_text_box[0] - M_MARGIN, lore_text_box[1] - M_MARGIN, lore_text_box[2] + M_MARGIN, lore_text_box[3] + M_MARGIN),
+        txt = break_text(f_txt(champ.champ_lore()), FONT50, draw, WIDTH - 2 * L_MARGIN)
+        lore_text_box = draw.multiline_textbbox((0, 0), txt, font=FONT50, spacing=SPACING)
+        lore_box = (M_MARGIN, champ_icon_box[3] + XL_MARGIN, WIDTH - M_MARGIN, champ_icon_box[3] + XL_MARGIN + lore_text_box[3] + 2 * S_MARGIN)
+        draw.rectangle(lore_box,
                        fill=(bg[0] - 10, bg[1] - 10, bg[2] - 10),
                        outline=(bg[0] + 20, bg[1] + 20, bg[2] + 20))
-        draw.multiline_text(coords, txt, font=FONT40, spacing=XS_MARGIN)
-        lore_box = [lore_text_box[0] - M_MARGIN, lore_text_box[1] - M_MARGIN, lore_text_box[2] + M_MARGIN, lore_text_box[3] + M_MARGIN]
+        draw.multiline_text((lore_box[0] + S_MARGIN, lore_box[1] + S_MARGIN), txt, font=FONT50, spacing=SPACING)
 
         # -------------------- base stats ---------------------
-        stats_space = 30
         stats_start_y = lore_box[3] + L_MARGIN
         ad = str(champ.attack_damage())
 
@@ -108,14 +116,10 @@ def ChI_image(lolData: LoLData, champ: Champ):
         # passive
         passive_box = [lore_box[0], lore_box[3] + XXL_MARGIN, lore_box[2], lore_box[3] + XXL_MARGIN + 300]
 
-        draw.rectangle((passive_box[0], passive_box[1], passive_box[2], passive_box[3]),
-                       fill=(bg[0] - 10, bg[1] - 10, bg[2] - 10),
-                       outline=(bg[0] + 20, bg[1] + 20, bg[2] + 20))
-
-        txt_box = draw.textbbox((0, 0), 'P', font=FONT50)
-        p_y = passive_box[1] + (passive_box[3] - SPELL_IMG_SIZE - S_MARGIN - passive_box[1] - S_MARGIN) / 2 - txt_box[3] / 2
-        x = passive_box[0] + S_MARGIN + SPELL_IMG_SIZE / 2 - txt_box[2] / 2
-        draw.text((x, p_y), 'Passive', font=FONT50)
+        txt_box = draw.textbbox((0, 0), '[P]', font=FONT50)
+        p_y = passive_box[1] + S_MARGIN
+        x = passive_box[0] + S_MARGIN
+        draw.text((x, p_y), '[P]', font=FONT50)
 
         x = passive_box[0] + S_MARGIN
         y = passive_box[3] - SPELL_IMG_SIZE - S_MARGIN
@@ -124,15 +128,19 @@ def ChI_image(lolData: LoLData, champ: Champ):
                     (SPELL_IMG_SIZE, SPELL_IMG_SIZE),
                     img,
                     mask=False)
-        passive_img_box = [x, y, x + 170, y + 170]
+        passive_img_box = [x, y, x + SPELL_IMG_SIZE, y + SPELL_IMG_SIZE]
 
-        draw.text((passive_img_box[2] + L_MARGIN, p_y), champ.passive_name(), font=FONT50)
+        draw.text((passive_img_box[2] + S_MARGIN, p_y), champ.passive_name(), font=FONT50)
 
-        max_width = passive_box[2] - passive_box[0] - 2 * S_MARGIN - L_MARGIN
-        txt = break_text(champ.passive_description(), font=FONT40, draw=draw, max_width=max_width)
-        x = passive_img_box[2] + L_MARGIN
+        max_width = passive_box[2] - passive_img_box[2] - 2 * M_MARGIN
+        txt = break_text(f_txt(champ.passive_description()), font=FONT40, draw=draw, max_width=max_width)
+        x = passive_img_box[2] + M_MARGIN
         y = passive_img_box[1]
-        draw.multiline_text()
+        draw.multiline_text((x, y), txt, font=FONT40, spacing=SPACING)
+        passive_text_box = draw.multiline_textbbox((x, y), txt, font=FONT40, spacing=SPACING)
+
+        draw.rectangle((passive_box[0], passive_box[1], passive_box[2], passive_text_box[3] + S_MARGIN),
+                       fill=(bg[0] - 10, bg[1] - 10, bg[2] - 10))
 
         # TODO first compute all positions and boxes, then display elements
 
@@ -163,3 +171,62 @@ def ChI_image(lolData: LoLData, champ: Champ):
         HEIGHT = new_height
 
         return img
+
+
+def draw_ability(draw: ImageDraw, ability: str, box: tuple[int, int, int], champ: Champ):
+    """
+    :param draw: the canvas to draw on
+    :param ability: one of 'passive', 'q', 'w', 'e', 'r'
+    :param box: x, y and width
+    :param champ: a Champion object from LoLChamp.py
+    draws the ability on the designed area of the canvas
+    """
+
+    letter = ''
+    desc = ''
+    title = ''
+    cd = ''
+    stats = ''
+    match ability:
+        case 'passive':
+            letter = '[P]'
+            desc = champ.passive_description()
+            title = champ.passive_name()
+            cd = champ.passive_cooldown()
+            stats = champ.passive_stats()
+        case 'q':
+            letter = f'[{ability.upper()}]'
+            desc = champ.q_description()
+            title = champ.q_name()
+            cd = champ.q_cooldown()
+            stats = champ.q_stats()
+        case 'w':
+            letter = f'[{ability.upper()}]'
+            desc = champ.w_description()
+            title = champ.w_name()
+            cd = champ.w_cooldown()
+            stats = champ.w_stats()
+        case 'e':
+            letter = f'[{ability.upper()}]'
+            desc = champ.e_description()
+            title = champ.e_name()
+            cd = champ.e_cooldown()
+            stats = champ.e_stats()
+        case 'r':
+            letter = f'[{ability.upper()}]'
+            desc = champ.r_description()
+            title = champ.r_name()
+            cd = champ.r_cooldown()
+            stats = champ.r_stats()
+
+    cd_box = draw.textbbox((0, 0), cd, font=FONT40)
+
+    x_ability, y_ability = box[0] + S_MARGIN, box[1] + S_MARGIN
+
+    x_title, y_title = box[0] + SPELL_IMG_SIZE + S_MARGIN, y_ability
+
+    x_img, y_img = x_ability, y_ability + draw.textbbox((0, 0), '[Q]')[3] + M_MARGIN
+
+    x_desc, y_desc = x_img, y_img + SPELL_IMG_SIZE + M_MARGIN
+
+    x_cd, y_cd = box[2] - cd_box[2] - S_MARGIN, box[3] - cd_box[3] - S_MARGIN
